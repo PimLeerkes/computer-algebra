0601196b61a5ce1b1a59e4b4342b25eb471444a5
PimLeerkes <83347609+PimLeerkes@users.noreply.github.com>
2025-05-27 12:11:42 +0200

    Update project.m

--- project.m
+++ project.m
@@ -60,16 +60,15 @@ function LaurentSeriesAroundPoint(f, z0, p)
     //z0 needs to be a point in the complex numbers
     //f needs to be a rational function
     //assert Parent(z0) eq ComplexField();
-    //print(Parent(z0));
 
     //we first need to deretmine the singularities?
     
     //substitute t := z - z0 => z := t + z0
     K<t> := Parent(f);
     f_sub := Evaluate(f, t + z0);
- 
+
     //we first need to calculate the partial fraction decomposition (where all denominators are linear (to a power))
-    decomposition := MyPartialFractionDecomposition(f);
+    decomposition := MyPartialFractionDecomposition(f_sub);
 
     //for each part in the decomposition we determine the geometric series and add them together
     laurent_expansion := 0;
@@ -79,11 +78,24 @@ function LaurentSeriesAroundPoint(f, z0, p)
         laurent_expansion +:= geom;
     end for;
 
-    //substitute back
-    K<z> := Parent(laurent_expansion);
-    laurent_expansion := Evaluate(laurent_expansion, z - z0);
+    //we return the laurent expansion together with the point, which can later be used to pretty print the series
+    return <laurent_expansion,z0>;
+end function;
 
-    return laurent_expansion;
+//the main function of this project. prints all the relevent information about the laurent/taylorexpansion of a given rational function
+function LaurentAnalysis(f,z0, p)
+    //determine singularities
+
+    //determine domains
+
+    //calculate series around the domains
+
+    //calculate residue
+
+    //print everything
+
+    //return the important information?
+    return f;
 end function;
 
 //TODO elementary trancendental functions/inverses of them (automatic if we don't implement ourselves)
@@ -95,17 +107,21 @@ end function;
 function SeriesEqual(f,z0,p) 
     //helper function to automatically test equality of our own implementation and magmas implementation
 
-    //we first compute the magma laurent series around the point
+    //we first compute the magma laurent series around the point (how to do this in a nice way?)
     K<t> := Parent(f);
+    //print(K);
     f_sub := Evaluate(f, t + z0);
-    L := LaurentSeriesRing(K);
+    Q := Rationals();
+    L := LaurentSeriesRing(Q,p);
     magma_series := L ! f_sub;
-    K<z> := Parent(magma_series);
-    magma_series := Evaluate(magma_series, z - z0);
 
-    my_series := L ! LaurentSeriesAroundPoint(f, z0, p);
-    //print(magma_series);
-    //print(LaurentSeriesAroundPoint(f, z0, p));
+    //K<z> := Parent(magma_series);
+    //magma_series := Evaluate(magma_series, z - z0);
+
+    //then we cast our laurent series to a magma laurent series to compare them
+    my_series := LaurentSeriesAroundPoint(f, z0, p)[0];
+    my_series := L ! my_series;
+
     if [Coefficient(my_series, i) : i in [-p/2..p/2]] eq
        [Coefficient(magma_series, i) : i in [-p/2..p/2]] then
        return true;
@@ -116,7 +132,8 @@ function SeriesEqual(f,z0,p)
     end if;
 end function;
 
-TestLaurentSeriesAroundPoint := procedure()
+AutomaticTestLaurentSeriesAroundPoint := procedure()
+    //for automatic unit testing
     //we test our implementations with rational functions constructed here
     Q := Rationals();
     K<z> := RationalFunctionField(Q);
@@ -124,12 +141,25 @@ TestLaurentSeriesAroundPoint := procedure()
     //TODO how to test around a point?
     //TODO z0 must be complex
 
+    //tests around 0
     assert SeriesEqual(1/(1-z), 0, 20);
-    //assert SeriesEqual(1/z, 0, 20);
-    //assert SeriesEqual((z - 3)/(z^2 + 1), 0, 20); 
-    //assert SeriesEqual(1/z^3, 0, 20); 
-    //assert SeriesEqual(1/(z^2+2*z), 0, 20);
+    assert SeriesEqual(1/z, 0, 20);
+    assert SeriesEqual((z - 3)/(z^2 + 1), 0, 20); 
+    assert SeriesEqual(1/z^3, 0, 20); 
+    assert SeriesEqual(1/(z^2+2*z), 0, 20);
 
-    //tests around a different point
+    //tests around a different point than 0
+    assert SeriesEqual(z, 1, 20);
+    assert SeriesEqual(1/z, 1, 20);
     assert SeriesEqual(1/(z^2+2*z), 1, 20);
+    assert SeriesEqual((z - 3)/(z^2 + 1), 1/2, 20); 
+end procedure;
+
+
+ManualTestLaurentSeriesAroundPoint := procedure()
+    //to test the whole laurent analysis manually
+    Q := Rationals();
+    K<z> := RationalFunctionField(Q);
+    p := 1/(1-z);
+    LaurentAnalysis(p,20);
 end procedure;
